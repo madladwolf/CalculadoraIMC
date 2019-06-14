b730dd0c1b808e00007a8982f58e4c16af2fdb9a
madladwolf <48660166+madladwolf@users.noreply.github.com>
2019-06-14 09:36:08 +0100

    Add files via upload

---
+++ CalcIMC/CalcIMC/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace CalcIMC
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txt_peso = new System.Windows.Forms.TextBox();
+            this.txt_altura = new System.Windows.Forms.TextBox();
+            this.btn_calcular = new System.Windows.Forms.Button();
+            this.lbl_resultado = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(31, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Peso";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 39);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(34, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Altura";
+            // 
+            // txt_peso
+            // 
+            this.txt_peso.Location = new System.Drawing.Point(51, 5);
+            this.txt_peso.Name = "txt_peso";
+            this.txt_peso.Size = new System.Drawing.Size(100, 20);
+            this.txt_peso.TabIndex = 2;
+            this.txt_peso.TextChanged += new System.EventHandler(this.Txt_peso_TextChanged);
+            // 
+            // txt_altura
+            // 
+            this.txt_altura.Location = new System.Drawing.Point(51, 32);
+            this.txt_altura.Name = "txt_altura";
+            this.txt_altura.Size = new System.Drawing.Size(100, 20);
+            this.txt_altura.TabIndex = 3;
+            this.txt_altura.TextChanged += new System.EventHandler(this.Txt_altura_TextChanged);
+            // 
+            // btn_calcular
+            // 
+            this.btn_calcular.Location = new System.Drawing.Point(158, 5);
+            this.btn_calcular.Name = "btn_calcular";
+            this.btn_calcular.Size = new System.Drawing.Size(76, 47);
+            this.btn_calcular.TabIndex = 4;
+            this.btn_calcular.Text = "button1";
+            this.btn_calcular.UseVisualStyleBackColor = true;
+            this.btn_calcular.Click += new System.EventHandler(this.Btn_calcular_Click);
+            // 
+            // lbl_resultado
+            // 
+            this.lbl_resultado.AutoSize = true;
+            this.lbl_resultado.Location = new System.Drawing.Point(241, 22);
+            this.lbl_resultado.Name = "lbl_resultado";
+            this.lbl_resultado.Size = new System.Drawing.Size(0, 13);
+            this.lbl_resultado.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(460, 67);
+            this.Controls.Add(this.lbl_resultado);
+            this.Controls.Add(this.btn_calcular);
+            this.Controls.Add(this.txt_altura);
+            this.Controls.Add(this.txt_peso);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txt_peso;
+        private System.Windows.Forms.TextBox txt_altura;
+        private System.Windows.Forms.Button btn_calcular;
+        private System.Windows.Forms.Label lbl_resultado;
+    }
+}
+
---
+++ CalcIMC/CalcIMC/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalcIMC
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            btn_calcular.Enabled = false;
+        }
+
+        private void Btn_calcular_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string altura = txt_altura.Text.Replace(',', '.');
+                string peso = txt_peso.Text.Replace(',', '.');
+                double pesoDouble = double.Parse(peso);
+                double alturaDouble = double.Parse(altura);
+                double resultado = pesoDouble / Math.Pow(alturaDouble, 2);
+                lbl_resultado.Text = resultado.ToString();
+            }
+            catch
+            {
+                lbl_resultado.Text = "!!Insira Apenas Numeros!!";
+            }
+            
+        }
+
+        private void Txt_altura_TextChanged(object sender, EventArgs e)
+        {
+            btn_calcular.Enabled = !String.IsNullOrEmpty(txt_altura.Text) && !String.IsNullOrEmpty(txt_peso.Text);
+        }
+
+        private void Txt_peso_TextChanged(object sender, EventArgs e)
+        {
+            btn_calcular.Enabled = !String.IsNullOrEmpty(txt_altura.Text) && !String.IsNullOrEmpty(txt_peso.Text);
+        }
+    }
+}
